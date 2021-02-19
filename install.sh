3eb1cdbe4ca00f463ce70d2a73bfc05e8f1e7bff
RahulSharma468 <52377774+Rahulsharma468@users.noreply.github.com>
2021-02-19 18:51:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#!/bin/bash
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# -----------------CURL-----------------
+echo "Installing CURL"
+sudo apt install curl -y
 
+# -----------------NODE-----------------
+echo "Installing NODE"
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+chmod +x nodesource_setup.sh
+sudo ./nodesource_setup.sh
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt install build-essential -y
+
+# ----------------MONGODB---------------
+echo "Installing MONGODB"
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt update
-
-sudo apt install -y mongodb-org
-
-sudo systemctl start mongod
-
-sudo systemctl status mongod
-
+sudo apt install mongodb-org -y
+sudo systemctl start mongod.service
 sudo systemctl enable mongod
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-chmod +rwx ~/.nvm/nvm.sh
-
-. ~/.nvm/nvm.sh
-
-nvm install node
